16f8a2f942b82e0cf33fe8ea41e1c370999ac3fb
24vekk <memkirill138@gmail.com>
2024-10-06 13:25:04 +0300

    Add files via upload
    add sprite player

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from scripts.player import Player
 
 
 flags = pygame.RESIZABLE | pygame.SCALED
@@ -11,8 +12,10 @@ background = pygame.image.load('images\\background.png')
 background = pygame.transform.scale(background, (800, 600))
 enemy = pygame.image.load('images\\enemy.png')
 bullet = pygame.image.load('images\\bullet.png')
-player = pygame.image.load('images\\player.png')
+player_image = pygame.image.load('images\\player.png')
 
+player_image = pygame.transform.scale(player_image, (75, 75))
+player = Player(400, 550, player_image, 5)
 
 game = True
 while game:
@@ -20,9 +23,10 @@ while game:
         if event.type == pygame.QUIT:
             game = False
 
+    player.update()
 
     window.blit(background, (0, 0))
-
+    player.render(window)
 
     pygame.display.update()
     clock.tick(FPS)
